083b5697921215b072d271aa3a1067b2d393fa85
kuramagod <156893596+kuramagod@users.noreply.github.com>
2025-08-04 23:14:55 +0300

    Сделана обслуживание статических файлов через ASGI.

--- config/asgi.py
+++ config/asgi.py
@@ -1,20 +1,20 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 django_asgi_app = get_asgi_application()
 
 from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
+        "http": ASGIStaticFilesHandler(django_asgi_app),
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
